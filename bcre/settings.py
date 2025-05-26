2be6c2943570e342f47945f15466b6d1b3540f7b
JamesTong <kachuntong01@gmail.com>
2025-05-26 14:45:27 +0800

    Step 2 : create pages app

--- bcre/settings.py
+++ bcre/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [ # process Login Logout
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'bcre.urls' #(folder name).(attribute)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #register templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
