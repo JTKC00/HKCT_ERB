99de6d2c74dc87215e383904868c38a41467843e
JamesTong <kachuntong01@gmail.com>
2025-05-27 16:33:10 +0800

    Step 3: pages app done

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("<h1>Hello World</h1>")   #testing
+    pages_url = reverse('pages:index')  # get the url of index page
     return render(request,'pages/index.html')
 
 def about(request):
+    pages_url = reverse('pages:about')  # get the url of about page
     return render(request,'pages/about.html')
+    
