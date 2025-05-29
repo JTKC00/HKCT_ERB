9fafff875a99ba5e9663f3e45c305a4c86d28e0e
JamesTong <kachuntong01@gmail.com>
2025-05-29 17:21:25 +0800

    Step 6:register model

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Listing # Import the Listing model from models.py
-
-admin.site.register(Listing) 
-# Register the Listing model with the admin site to manage listings through the Django admin interface
+from django.forms import NumberInput  # Import NumberInput for custom form field rendering
+from django.db import models  # Import models for database interaction
+class ListingAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published', 'price', 'list_date', 'realtor'  # Fields to display in the admin list view
+    list_display_links = 'id', 'title'  # Fields that are clickable links in the admin list view
+    search_fields = 'title', 'description', 'address', 'price' # Fields to search in the admin interface
+    list_filter = 'realtor',  # Filters to apply in the admin interface
+    list_per_page = 25  # Number of listings per page in the admin list view
+    list_editable = 'price', 'is_published'  # Fields that can be edited directly in the admin list view
+    ordering = ['-id'] # Order listings by ID in descending order
+    prepopulated_fields = {'title': ('title',)}  
+    formfield_overrides = {
+        models.IntegerField: {'widget': NumberInput(attrs= {'size': '10'})}}  # Override the form field for price to use a NumberInput widget with a specific size
+    show_facets = admin.ShowFacets.ALWAYS  # Always show facets in the admin interface
+admin.site.register(Listing, ListingAdmin)  # Register the Listing model with the admin site using the ListingAdmin configuration
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Realtor  # Import the Realtor model from models.py
 # Register your models here.
-admin.site.register(Realtor)  # Register the Realtor model with the admin site to manage realtors through the Django admin interface
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')  # Fields to display in the admin list view
+    list_display_links = ('id', 'name')  # Fields that are clickable links in the admin list view
+    search_fields = ('name',)  # Fields to search in the admin interface
+    list_per_page = 25  # Number of realtors per page in the admin list view
+
+
+admin.site.register(Realtor, RealtorAdmin)  
+# Register the Realtor model with the admin site using the RealtorAdmin configuration
