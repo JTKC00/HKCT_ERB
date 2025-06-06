fc3c888797365511f2cfe108a486f7178cef9bc7
JamesTong <kachuntong01@gmail.com>
2025-06-06 16:32:17 +0800

    Step 15 : finish

---
+++ static/js/modal.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    //When the model is shown
+$("#deleteConfirmMedal").on("show.bs.modal", function (event){
+    const button = $(event.relatedTarget);
+    const contactId = button.data("id");
+
+    //Update modal content
+    $("#modal-contact-id").text(contactId);
+
+    //Set the delete URL
+    const deleteUrl = `{% url 'contacts:delete_contact' 0 %}`.replace(
+        "0",
+        contactId
+    );
+    $("#confirmDeleteBtn").attr("href", deleteUrl);
+});
+
+//When delete button is clicked
+$("#confirmDeleteBtn").click(function (e){
+    // Close the modal
+    $("#deleteConfirmModal").modal("hide");
+
+    //Optional: Add a small delay to let the modal close before redirecting
+    setTimeout(function(){
+        window.location.href = $("#confirmDeleteBtn").attr("href");
+    }, 300);
+});
+});
